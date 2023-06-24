0e66c15b3edfa90e546856c525f488152525340a
mjkareem <mujahid_abdulkareem@seneca2.lehre.hwr-berlin.de>
2023-06-24 20:53:52 +0200

    Exercise 3

--- mapper.py
+++ mapper.py
@@ -33,6 +33,10 @@ for line in sys.stdin:
     # strip removes whitespaces and new lines at the beginning and end of the line
     # the result is a tuple with 6 elements
     data = line.strip().split("\t")
+    
+    #When a user does not fill all six element, an error is given
+    if len(data) != 6:
+	raise ValueError("Invalid input format. Expected six elements.")
 
     # store the 6 elements of the tuple in seperate variables
     date, time, item, category, sales, payment = data
